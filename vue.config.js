4a42a4a7c15fd29490585bee33951776454b6fb1
依栏听风 <yilantingfeng@yilantingfengdeMacBook-Pro.local>
2021-07-19 19:54:04 +0800

    master

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+module.exports = {
+    publicPath: "./",
+    outputDir: "dist",
+    assetsDir: "static",
+    indexPath: "index.html",
+    filenameHashing: true,
+    productionSourceMap: false,
+    lintOnSave: false,
+}
